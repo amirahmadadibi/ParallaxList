74bf4b942789c430121e5bd4f0ed13ff0f0cd1ca
amirahmadadibi <amirahmadadibi@gmail.com>
2019-10-09 13:11:19 +0330

    desing cardview poster

---
+++ app/src/main/java/com/amirahmadadibi/projects/androidparallaxrecyclerview/adapter/ShowAdapter.kt
@@ -0,0 +1,15 @@
+package com.amirahmadadibi.projects.androidparallaxrecyclerview.adapter
+
+import com.amirahmadadibi.projects.androidparallaxrecyclerview.model.Show
+
+/**
+ * Created by Amirahmad Adibi.
+ * Android Parallax Recyclerview | Copyrights 2019-10-09.
+ */
+class ShowAdapter(private val items: List<Show>){
+    init {
+//        setHasStableIds(true)
+    }
+
+
+}
