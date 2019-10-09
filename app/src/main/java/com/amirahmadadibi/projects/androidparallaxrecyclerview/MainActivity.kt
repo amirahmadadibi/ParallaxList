519bdc65bc45f60759eb535e2fbde471b2a630b6
amirahmadadibi <amirahmadadibi@gmail.com>
2019-10-09 16:14:43 +0330

    complete parallax

--- app/src/main/java/com/amirahmadadibi/projects/androidparallaxrecyclerview/MainActivity.kt
+++ app/src/main/java/com/amirahmadadibi/projects/androidparallaxrecyclerview/MainActivity.kt
@@ -2,13 +2,35 @@ package com.amirahmadadibi.projects.androidparallaxrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.amirahmadadibi.projects.androidparallaxrecyclerview.adapter.ShowAdapter
+import com.amirahmadadibi.projects.androidparallaxrecyclerview.extension.setupParallaxScrollListener
+import com.amirahmadadibi.projects.androidparallaxrecyclerview.model.Show
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val moviewList = mutableListOf<Show>()
+        moviewList +=   Show(R.string.title1,R.string.description1,R.drawable.photo1)
+        moviewList +=   Show(R.string.title2,R.string.description2,R.drawable.photo2)
+        moviewList +=   Show(R.string.title3,R.string.description3,R.drawable.photo3)
         setContentView(R.layout.activity_main)
+        recyclerview.apply {
+            // Create and attach the adapter
+            adapter = ShowAdapter(moviewList)
 
+            // Configure to be a horizontal list
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+            // Attach a snap helper, more on that below
+            PagerSnapHelper().attachToRecyclerView(this)
+
+            // Where most of the parallax magic happens
+            setupParallaxScrollListener()
+        }
 
     }
 }
--- app/src/main/java/com/amirahmadadibi/projects/androidparallaxrecyclerview/adapter/ShowAdapter.kt
+++ app/src/main/java/com/amirahmadadibi/projects/androidparallaxrecyclerview/adapter/ShowAdapter.kt
@@ -1,15 +1,31 @@
 package com.amirahmadadibi.projects.androidparallaxrecyclerview.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.contentValuesOf
+import androidx.recyclerview.widget.RecyclerView
+import com.amirahmadadibi.projects.androidparallaxrecyclerview.R
 import com.amirahmadadibi.projects.androidparallaxrecyclerview.model.Show
 
 /**
  * Created by Amirahmad Adibi.
  * Android Parallax Recyclerview | Copyrights 2019-10-09.
  */
-class ShowAdapter(private val items: List<Show>){
+class ShowAdapter(private val items: List<Show>) : RecyclerView.Adapter<ShowViewHolder>() {
     init {
-//        setHasStableIds(true)
+        setHasStableIds(true)
     }
+    override fun getItemCount(): Int = items.size
+    override fun getItemId(position: Int): Long = items[position].hashCode().toLong()
+    override fun onCreateViewHolder(parent: ViewGroup, vt: Int):ShowViewHolder{
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_shop,parent,false)
+        return ShowViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ShowViewHolder, position: Int) = holder.bind(items[position])
+
+
 
 
 }
